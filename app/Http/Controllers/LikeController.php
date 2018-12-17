a98f59fae8d50e9fc879bc7fc159a9c96df7447e
MansoorAhmedShariff <45815354+MansoorAhmedShariff@users.noreply.github.com>
2018-12-17 19:02:09 +0530

    Likes Create and Delete

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,83 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Model\Like;
+use App\Model\Reply;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+   public function Like(Reply $reply, Request $request){
+        $reply->like()->create([
+            //'user_id' => auth()->id()
+            'user_id' => $request->user_id
+           ]);
+        return response()->json("Added Successfully", 200);
+   }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Model\Like  $likes
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Like $likes)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Like  $likes
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Like $likes)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Like  $likes
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Like $likes)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Model\Like  $likes
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Like $likes)
-    {
-        //
-    }
+   public function Unlike(Reply $reply, Request $request){
+        $reply->like()->where('user_id', $request->user_id)->first()->delete();
+        return response()->json("Deleted Successfully", 200);
+   }
 }
